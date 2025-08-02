8b5dcb18351d2d67dbd167c4a9f4c56c4fda7b2f
joaorgd <joaorobertoguedesdantas@gmail.com>
2025-08-02 10:07:58 -0300

    criação de records na pasta dto

---
+++ liter-alura/src/main/java/com/alura/literalura/dto/DadosLivro.java
@@ -0,0 +1,13 @@
+// Pacote: com.alura.literalura.dto
+package com.alura.literalura.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosLivro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DadosAutor> autores,
+        @JsonAlias("languages") List<String> idiomas
+) {}
